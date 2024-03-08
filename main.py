46acc85d87f145262172befb9b3c0efd1369196f
Ren <152279955+ChristianJude23@users.noreply.github.com>
2024-03-08 20:07:42 +0800

    Major Update
    Added a GUI

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
-def is_palindrome(n):
-    if n == n[::-1]:
-        print(f"{n} is a palindrome!")
-    else:
-        print(f"{n} is not a palindrome!")
+import tkinter as tk
 
-string = input("Enter a string: ").lower()
-is_palindrome(string)
+def is_palindrome():
+    txt = ent_text.get().lower()
+    if txt == "":
+        pass
+    else:
+        if txt == txt[::-1]:
+            lbl_txt.config(text="Palindrome", bg="Green")
+        else:
+            lbl_txt.config(text="Not a Palindrome", bg="Red", fg="White")
+
+
+
+#win Config
+win = tk.Tk()
+win.title("Palindrome Checker")
+win.geometry("300x200")
+win.resizable(False, False)
+win.config(bg="#D6A184")
+
+#widgets
+ent_text = tk.Entry(win, width=20, font=("Helvetica", 16))
+ent_text.pack(pady=20)
+
+btn_enter = tk.Button(win, text="Enter", font=("Helvetica", 12), command=is_palindrome, width=10).pack()
+
+lbl_txt = tk.Label(win, text="Palindrome or Not?", font=("Helvetica", 14))
+lbl_txt.pack(pady=20)
+
+win.mainloop()
